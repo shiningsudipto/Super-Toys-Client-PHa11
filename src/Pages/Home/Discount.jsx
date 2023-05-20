4e48e931b895d0d469558a9afd03a72bd238693d
Sudipto Das <shiningsudipto@gmail.com>
2023-05-20 19:54:28 +0600

    AOS applied

--- src/Pages/Home/Discount.jsx
+++ src/Pages/Home/Discount.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import img1 from '../../assets/Discount/d1.jpg'
 import img2 from '../../assets/Discount/d2.jpg'
 
 const Discount = () => {
+    useEffect(() => {
+        AOS.init({ offset: 100, duration: 400, easing: 'ease-in-out', delay: 200 })
+    }, [])
     return (
         <div className='lg:my-14'>
             <div className='text-center '>
@@ -10,7 +15,7 @@ const Discount = () => {
                 <h4 className='text-3xl font-semibold mb-6'>Collect before ran out.</h4>
             </div>
             <div className='grid lg:grid-cols-2 grid-cols-1 lg:gap-6 lg:mx-10 lg:space-y-0 space-y-6'>
-                <div className="card bg-base-100 shadow-xl image-full">
+                <div data-aos="zoom-in" className="card bg-base-100 shadow-xl image-full">
                     <figure><img src={img1} alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="lg:text-6xl font-bold">Collection</h2>
@@ -20,7 +25,7 @@ const Discount = () => {
                         </div>
                     </div>
                 </div>
-                <div className="card bg-base-100 shadow-xl image-full">
+                <div data-aos="zoom-in" className="card bg-base-100 shadow-xl image-full">
                     <figure><img src={img2} alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="lg:text-6xl font-bold">Collection</h2>
--- src/Pages/Home/Gallery.jsx
+++ src/Pages/Home/Gallery.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import img1 from '../../assets/Gallery/g1.jpg'
 import img2 from '../../assets/Gallery/g2.jpg'
 import img3 from '../../assets/Gallery/g3.jpg'
@@ -7,6 +9,9 @@ import img5 from '../../assets/Gallery/g5.jpg'
 import img6 from '../../assets/Gallery/g6.jpg'
 
 const Gallery = () => {
+    useEffect(() => {
+        AOS.init({ offset: 200, duration: 400, easing: 'ease-in-out', delay: 200 })
+    }, [])
     return (
         <div className='mb-14'>
             <div className='text-center my-6'>
@@ -15,7 +20,7 @@ const Gallery = () => {
             </div>
             <div className="grid lg:grid-cols-3 grid-cols-1">
                 {/* Image 1 */}
-                <div className="">
+                <div data-aos="fade-right" className="">
                     <div className="relative group">
                         <img src={img1} alt="Image 1" className="w-full" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
@@ -27,7 +32,7 @@ const Gallery = () => {
                 </div>
 
                 {/* Image 2 */}
-                <div className="">
+                <div data-aos="fade-up" className="">
                     <div className="relative group">
                         <img src={img2} alt="Image 2" className="w-full" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
@@ -39,7 +44,7 @@ const Gallery = () => {
                 </div>
 
                 {/* Image 3 */}
-                <div className="">
+                <div data-aos="fade-left" className="">
                     <div className="relative group">
                         <img src={img3} alt="Image 3" className="w-full" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
@@ -50,7 +55,7 @@ const Gallery = () => {
                     </div>
                 </div>
                 {/* Image 4 */}
-                <div className="">
+                <div data-aos="fade-right" className="">
                     <div className="relative group">
                         <img src={img4} alt="Image 3" className="w-full" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
@@ -61,7 +66,7 @@ const Gallery = () => {
                     </div>
                 </div>
                 {/* Image 5 */}
-                <div className="">
+                <div data-aos="fade-up" className="">
                     <div className="relative group">
                         <img src={img5} alt="Image 3" className="w-full" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
@@ -72,7 +77,7 @@ const Gallery = () => {
                     </div>
                 </div>
                 {/* Image 6 */}
-                <div className="">
+                <div data-aos="fade-left" className="">
                     <div className="relative group">
                         <img src={img6} alt="Image 3" className="w-full" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
