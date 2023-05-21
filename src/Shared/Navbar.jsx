749473938e535a512c3dc87d4b76b90491cdb9a2
Sudipto Das <shiningsudipto@gmail.com>
2023-05-21 13:18:42 +0600

    nav button styled

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -36,14 +36,14 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-semibold">
                             {menuLinks}
                         </ul>
                     </div>
                     <Link className=""><img className='h-10' src={logo} alt="" /></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                    <ul className="menu menu-horizontal px-1 font-semibold">
                         {menuLinks}
                     </ul>
                 </div>
@@ -57,10 +57,10 @@ const Navbar = () => {
                                     </div>
                                 }
 
-                                <button onClick={handleLogOut}>Logout</button>
+                                <button className='border p-2 rounded-xl border-orange font-semibold hover:bg-orange hover:text-white' onClick={handleLogOut}>Logout</button>
                             </>
                             :
-                            <Link to="/signin">Login</Link>
+                            <Link className='border p-2 rounded-xl border-orange font-semibold hover:bg-orange hover:text-white' to="/signin">Login</Link>
                     }
                 </div>
             </div>
